456da43a707162c993b88a83ebd139b99ce396a8
kenoooken <keeen1103@gmail.com>
2019-12-09 21:27:05 +0900

    add post_image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,31 +2,92 @@ class PostsController < ApplicationController
 before_action :authenticate_user!, except: [:index]
     
   def new
-          @post = Post.new
+      @post = Post.new
+       @post = current_user.posts.build
   end
     
-    def create
-      
-    @post = Post.new(post_params)  
-    @post.user_id = current_user.id 
-    @post.save
-    redirect_to posts_path
-      
-  end
+    def edit
+               @post = Post.find_by(id: params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+    if @post.update(post_params)
+            flash[:notice] = "投稿が保存されました"
+           # render 'edit'
+            render "edit"
+     else
+            flash[:alert] = "しゃしんが　ないよ！"
+        #    render plain: @post.erros.inspect
+            render 'edit'
+        end
+    end
+    
+      def create
+        @post = current_user.posts.build(post_params)
+        @post.user_id = current_user.id
+        if @post.save
+
+          redirect_to "/posts/#{@post.id}"
+          flash[:notice] = "投稿が保存されました"
+        else
+            
+          #flash[:notice] = "投稿"
+          flash[:alert] = "しゃしんが　ないよ！"
+          render("posts/new")
+        end
+    end
 
   def index
-          @posts = Post.all
+      @posts = Post.all.order(created_at: :desc)
+      @posts = Post.page(params[:page]).per(5)
   end
 
   def show
-        @post = Post.find(params[:id])
-        @user = @post.user
+      @post = Post.find(params[:id])
+      @user = @post.user
+
   end
-
-
-private
-    def post_params
-      params.require(:post).permit(:body,:image) 
-    end
     
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/")
+  end
+    
+    private
+        def post_params
+            params.require(:post).permit(
+                :title,
+                :body,
+                :image,
+                
+                :second_title,
+                :second_body,
+                :second_link,
+                :second_image,
+                
+                :third_title,
+                :third_body,
+                :third_link,
+                :third_image,
+                
+                :fourth_title,
+                :fourth_body,
+                :fourth_link,
+                :fourth_image,
+                
+                :fifth_title,
+                :fifth_body,
+                :fifth_link,
+                :fifth_image,
+                
+                :sixth_title,
+                :sixth_body,
+                :sixth_link,
+                :sixth_image,
+                
+                ).merge(user_id: current_user.id)
+        end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, {presence: true, length: {maximum: 140}}
+
+
         belongs_to :user
         has_many :favorites
     
+        default_scope -> { order(created_at: :desc) }
+    
+    mount_uploader :image, ImageUploader
+    mount_uploader :second_image, ImageUploader
+    mount_uploader :image, ImageUploader
+    mount_uploader :image, ImageUploader
     mount_uploader :image, ImageUploader
     
     def favorited_by?(user)
